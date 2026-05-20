055c6720b3fc690195cc0ef607cdc30fe4738056
Han Hossain <hanhossain@outlook.com>
2026-05-19 23:41:01 -0500

    move routes

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,14 @@
+mod routes;
+
 use actix_web::dev::Server;
-use actix_web::{App, HttpResponse, HttpServer, web};
-use serde::Deserialize;
+use actix_web::{App, HttpServer, web};
 use std::net::TcpListener;
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(Deserialize)]
-struct FormData {
-    _email: String,
-    _name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
+            .route("/health_check", web::get().to(routes::health_check))
+            .route("/subscriptions", web::post().to(routes::subscribe))
     })
     .listen(listener)?
     .run();
