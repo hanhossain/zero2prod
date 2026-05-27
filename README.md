4c66b5b5b7c7ecb35df5eef409cf4cc7d0d27a76
Han Hossain <hanhossain@outlook.com>
2026-05-27 17:11:36 -0500

    use docker compose for local

---
+++ README.md
@@ -0,0 +1,8 @@
+# zero2prod
+
+## local deployment
+
+```shell
+docker compose up --wait
+sqlx database setup
+```
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -42,7 +42,6 @@ done
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
-sqlx database create
-sqlx migrate run
+sqlx database setup
 
 >&2 echo "Postgres has been migrated, ready to go!"
