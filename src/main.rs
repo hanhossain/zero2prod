1703ee81de1bb8aa09d272183299775377cc1f15
Han Hossain <hanhossain@outlook.com>
2026-05-27 22:13:39 -0500

    fix database connection

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,7 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let mut configuration = get_configuration().expect("Failed to read configuration.");
-    configuration.database.database_name = Uuid::new_v4().to_string();
+    let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
