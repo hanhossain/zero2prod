3f5a31e272fb60acf227ef9778071416d7c88479
Han Hossain <hanhossain@outlook.com>
2026-05-27 20:37:52 -0500

    create db per test

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
+use uuid::Uuid;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let configuration = get_configuration().expect("Failed to read configuration.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
