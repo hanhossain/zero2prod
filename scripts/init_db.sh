7f9957940522130aafb622785a57680ea60baf42
Han Hossain <hanhossain@outlook.com>
2026-06-03 16:03:31 -0500

    remove POSTGRES_DB param

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -17,16 +17,15 @@ fi
 
 DB_USER=${POSTGRES_USER:=postgres}
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
+APP_DB_NAME="${POSTGRES_DB:=newsletter}"
 
 if [[ -z "${SKIP_DOCKER}" ]]
 then
   docker run \
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
     -p "${DB_PORT}":5432 \
     -d postgres:18 \
     postgres -N 1000
@@ -40,7 +39,7 @@ done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
-DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${APP_DB_NAME}
 export DATABASE_URL
 sqlx database setup
 
