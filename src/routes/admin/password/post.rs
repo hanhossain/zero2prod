9c4993ab958f5f7977c8e2097b53af05edd7d6c8
Han Hossain <hanhossain@outlook.com>
2026-06-16 01:20:19 -0500

    new passwords don't match

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,7 +1,8 @@
 use crate::session_state::TypedSession;
 use crate::utils::{e500, see_other};
 use actix_web::{HttpResponse, web};
-use secrecy::SecretString;
+use actix_web_flash_messages::FlashMessage;
+use secrecy::{ExposeSecret, SecretString};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -17,6 +18,13 @@ pub async fn change_password(
 ) -> Result<HttpResponse, actix_web::Error> {
     if session.get_user_id().map_err(e500)?.is_none() {
         return Ok(see_other("/login"));
+    }
+    if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
+        FlashMessage::error(
+            "You entered two different new passwords - the field values must match.",
+        )
+        .send();
+        return Ok(see_other("/admin/password"));
     }
     todo!()
 }
--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -22,3 +22,34 @@ async fn you_must_be_logged_in_to_change_your_password(pool: PgPool) {
         .await;
     assert_is_redirect_to(&response, "/login");
 }
+
+#[sqlx::test]
+async fn new_password_fields_must_match(pool: PgPool) {
+    let app = spawn_app(pool).await;
+    let new_password = Uuid::new_v4().to_string();
+    let another_new_password = Uuid::new_v4().to_string();
+
+    // login
+    app.post_login(&serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password,
+    }))
+    .await;
+
+    // try to change password
+    let response = app
+        .post_change_password(&serde_json::json!({
+            "current_password": &app.test_user.password,
+            "new_password": &new_password,
+            "new_password_check": &another_new_password
+        }))
+        .await;
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // follow the redirect
+    let html_page = app.get_change_password_html().await;
+    assert!(html_page.contains(
+        "<p><i>You entered two different new passwords - \
+        the field values must match.</i></p>"
+    ));
+}
