b1b789dfbd3e3b70e990d98930954059308e7540
Han Hossain <hanhossain@outlook.com>
2026-06-16 14:50:30 -0500

    change password works

--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -141,3 +141,47 @@ async fn current_password_must_be_valid(pool: PgPool) {
     let html_page = app.get_change_password_html().await;
     assert!(html_page.contains("<p><i>The current password is incorrect.</i></p>"));
 }
+
+#[sqlx::test]
+async fn changing_password_works(pool: PgPool) {
+    let app = spawn_app(pool).await;
+    let new_password = Uuid::new_v4().to_string();
+
+    // login
+    let login_body = &serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password,
+    });
+    let response = app.post_login(&login_body).await;
+    assert_is_redirect_to(&response, "/admin/dashboard");
+
+    // change password
+    let response = app
+        .post_change_password(&serde_json::json!({
+            "current_password": &app.test_user.password,
+            "new_password": &new_password,
+            "new_password_check": &new_password,
+        }))
+        .await;
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // follow the redirect
+    let html_page = app.get_change_password_html().await;
+    assert!(html_page.contains("<p><i>Your password has been changed.</i></p>"));
+
+    // logout
+    let response = app.post_logout().await;
+    assert_is_redirect_to(&response, "/login");
+
+    // follow the redirect
+    let html_page = app.get_login_html().await;
+    assert!(html_page.contains("<p><i>You have successfully logged out.</i></p>"));
+
+    // login using the new password
+    let login_body = serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &new_password
+    });
+    let response = app.post_login(&login_body).await;
+    assert_is_redirect_to(&response, "/admin/dashboard");
+}
