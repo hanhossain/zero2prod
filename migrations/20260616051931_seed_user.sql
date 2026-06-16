b059ebe9ce5b4a8cfbe9dd05a1ae7b41441260d0
Han Hossain <hanhossain@outlook.com>
2026-06-16 00:28:58 -0500

    seed admin user

---
+++ migrations/20260616051931_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(user_id, username, password_hash)
+VALUES (
+    'ae682421-0505-43e4-8c89-4ce5c91331f6',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$PK8m8Gy7lucNgyFbOzJOxw$nst7J5bKaj3ugADPJX1ER8KIBuO/fRsnA4xLFZSrplk'
+);
